32dd8e972da90c62f012347a7f596f4cba8eb75b
fishstiicks <loketteshimmer@gmail.com>
2024-11-04 10:22:27 -0600

    add bookings and spot migrations

---
+++ backend/db/models/spot.js
@@ -0,0 +1,80 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Spot extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Spot.init({
+    ownerId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    previewImage: DataTypes.STRING,
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    country: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lat: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      validate: {
+        isWithinLatRange(value) {
+          if (value < 90 || value > 90) {
+            throw new Error('Latitude must be within -90 and 90')
+          }
+        }
+      }
+    },
+    lng: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      validate: {
+        isWithinLngRange(value) {
+          if (value < -180 || value > 180) {
+            throw new Error('Longitude must be within -180 and 180')
+          }
+        }
+      }
+    },
+    name: {
+      type: DataTypes.STRING(49),
+      allowNull: false,
+      validate: {
+        isUnder50Char(value) {
+          if (value.length >= 50) {
+            throw new Error('Name must be less than 50 characters')
+          }
+        }
+      }
+    },
+    description: DataTypes.STRING,
+    price: DataTypes.INTEGER,
+    numReviews: DataTypes.INTEGER,
+    avgRating: DataTypes.FLOAT
+  }, {
+    sequelize,
+    modelName: 'Spot',
+  });
+  return Spot;
+};
---
+++ backend/routes/api/spots.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const user = require('../../db/models/spot');
+const router = express.Router();
+
+router.post(
+    '/',
+    async (req, res) => {
+        const { address, city, state, country, lat, lng, name, description, price } = req.body;
+        const spot = await Spot.create({ address, city, state, country, lat, lng, name, description, price });
+
+        const returnSpot = {
+            address: spot.address,
+            city: spot.city,
+            state: spot.state,
+            country: spot.country,
+            lat: spot.lat,
+            lng: spot.lng,
+            name: spot.name,
+            description: spot.description,
+            price: spot.price,
+        }
+
+        return res.json({spot: returnSpot})
+    }
+)
